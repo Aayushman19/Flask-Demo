d885e0184f67bfa6d1ae156a30661d9d5199f640
Aayushman Mishra <aayushmanmishra6@gmail.com>
2024-07-10 12:06:56 +0530

    added bill page

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-import json
 
 app = Flask(__name__)
 food_orders = []
 
 @app.route('/')
 def hello():
-    return render_template("food.html", message = "Order Now!")
+    return render_template("food.html")
 
-@app.route("/order", methods = ['POST'])
-def order():
+@app.route("/bill", methods = ['POST'])
+def bill():
     food = request.form["food"]
     food_orders.append(food)
-    return f"Your {food} is ready!"
-
-@app.route('/bill', methods = ['GET'])
-def bill():
-    return food_orders
+    return render_template("bill.html", orders = food_orders)
