03b9297e613af29d184c1159b15eb3a9e49334f1
Aayushman Mishra <aayushmanmishra6@gmail.com>
2024-07-10 14:31:28 +0530

    init updating bill

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import psycopg2
+def get_connection():
+	try:
+		return psycopg2.connect(
+			database="********",
+			user="postgres",
+			password="********",
+			host="localhost",
+			port=5432,
+		)
+	except:
+		return False
+conn = get_connection()
+if conn:
+	print("Connection to the PostgreSQL established successfully.")
+else:
+	print("Connection to the PostgreSQL encountered and error.")
 
 app = Flask(__name__)
 food_orders = []
