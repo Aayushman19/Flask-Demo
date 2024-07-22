4e94fef00b3d7a339865061e2170fa438c904e2d
Aayushman Mishra <aayushmanmishra6@gmail.com>
2024-07-22 14:12:21 +0530

    object code bug fix

--- Model/customer_order.py
+++ Model/customer_order.py
@@ -1,22 +1,20 @@
 class customer_order:
-    def __init__(self):
-        self.order_name = None
-        self.order_price = None
+    def __init__(self, food, price):
+        self.order_name = food
+        self.order_price = price
 
 
 
 if __name__ == '__main__':
     while True:
-        counter = 1
-        print(counter + ". Add item")
-        counter += 1
-        print(counter + ". Calculate Bill")
-        choice = input("Enter your item")
-        n = int(input())
+        print("1. Add item")
+        print("2. Calculate Bill")
+        choice = int(input("Enter your choice : "))
 
         if(int(choice) == 1):
-            food = input("Enter food item")
-            obj1 = customer_order(food)
+            food = input("Enter food item : ")
+            price = int(input("Enter Price : "))
+            obj1 = customer_order(food, price)
 
         if int(choice) == 2:
             pass
