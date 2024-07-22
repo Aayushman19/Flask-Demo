5cf93b9444a5514db278e681f908e59d288e3919
Aayushman Mishra <aayushmanmishra6@gmail.com>
2024-07-22 14:05:01 +0530

    adding object code

---
+++ Model/customer_order.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class customer_order:
+    def __init__(self):
+        self.order_name = None
+        self.order_price = None
+
+
+
+if __name__ == '__main__':
+    while True:
+        counter = 1
+        print(counter + ". Add item")
+        counter += 1
+        print(counter + ". Calculate Bill")
+        choice = input("Enter your item")
+        n = int(input())
+
+        if(int(choice) == 1):
+            food = input("Enter food item")
+            obj1 = customer_order(food)
+
+        if int(choice) == 2:
+            pass
---
+++ Model/total_bill.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class total_bill:
+    def __init__(self):
+        self.discount = None
+
+    def calculate(self, ):
+        pass
+
