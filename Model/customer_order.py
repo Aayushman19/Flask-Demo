84bfc20fbd5037ba1045ac51831c0058f8556e68
Aayushman Mishra <aayushmanmishra6@gmail.com>
2024-07-22 14:25:52 +0530

    object code fix

--- Model/customer_order.py
+++ Model/customer_order.py
@@ -1,7 +1,19 @@
 class customer_order:
+    amt = 0
     def __init__(self, food, price):
         self.order_name = food
         self.order_price = price
+
+class total_bill:
+    def __init__(self, amount):
+        self.discount = None
+        self.amount = amount
+
+    def calculate(self, discount, amount):
+        dis = (discount//amount)*100
+        amount -= dis
+        print("Final Bill : ", amount)
+
 
 
 
@@ -17,4 +29,4 @@ if __name__ == '__main__':
             obj1 = customer_order(food, price)
 
         if int(choice) == 2:
-            pass
+            obj2 = total_bill(obj1,)
--- Model/total_bill.py
+++
@@ -1,7 +0,0 @@
-class total_bill:
-    def __init__(self):
-        self.discount = None
-
-    def calculate(self, ):
-        pass
-
